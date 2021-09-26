24756fbb7ff057f245bc8b52186696e66c500c86
microwavedcola1 <microwavedcola@gmail.com>
2021-09-26 10:58:15 +0200

    pm2 + sig handling
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- py/mango_service_v3_py/api.py
+++ py/mango_service_v3_py/api.py
@@ -1,8 +1,6 @@
 import json
 import os
 import time
-from decimal import Decimal
-from threading import Timer
 from typing import List
 
 import httpx
@@ -41,41 +39,34 @@ class MangoServiceV3Client:
         else:
             self.BASE_URL = "http://localhost:3000/api"
 
-    @delayed(2)
     def get_open_positions(self) -> List[Position]:
         response = httpx.get(f"{self.BASE_URL}/positions", timeout=10.0)
         return parse_obj_as(List[Position], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_balances(self) -> List[Balance]:
         response = httpx.get(f"{self.BASE_URL}/wallet/balances", timeout=10.0)
         return parse_obj_as(List[Balance], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_markets(self) -> List[Market]:
         response = httpx.get(f"{self.BASE_URL}/markets", timeout=10.0)
         return parse_obj_as(List[Market], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_market_by_market_name(self, market_name: str) -> List[Market]:
         response = httpx.get(f"{self.BASE_URL}/markets/{market_name}", timeout=10.0)
         return parse_obj_as(List[Market], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_orderboook(self, market_name: str, depth: int = 30) -> Orderbook:
         response = httpx.get(
             f"{self.BASE_URL}/markets/{market_name}/orderbook?depth={depth}"
         )
         return parse_obj_as(Orderbook, json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_trades(self, market_name: str) -> List[Trade]:
         response = httpx.get(
             f"{self.BASE_URL}/markets/{market_name}/trades", timeout=10.0
         )
         return parse_obj_as(List[Trade], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_candles(
         self, market_name: str, resolution: int, start_time: int, end_time: int
     ) -> List[Candle]:
@@ -84,19 +75,16 @@ class MangoServiceV3Client:
         )
         return parse_obj_as(List[Candle], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_orders(self,) -> List[Order]:
         response = httpx.get(f"{self.BASE_URL}/orders", timeout=10.0)
         return parse_obj_as(List[Order], json.loads(response.text)["result"])
 
-    @delayed(2)
     def get_orders_by_market_name(self, market_name: str) -> List[Order]:
         response = httpx.get(
             f"{self.BASE_URL}/orders?market={market_name}", timeout=10.0
         )
         return parse_obj_as(List[Order], json.loads(response.text)["result"])
 
-    @delayed(2)
     def place_order(self, order: PlaceOrder) -> None:
         response = httpx.post(
             f"{self.BASE_URL}/orders", json=order.dict(by_alias=True), timeout=10.0
@@ -106,17 +94,14 @@ class MangoServiceV3Client:
         #         List[BadRequestError], json.loads(response.text)["errors"]
         #     )
 
-    @delayed(2)
     def cancel_order_by_client_id(self, client_id):
         response = httpx.delete(
             f"{self.BASE_URL}/orders/by_client_id/{client_id}", timeout=10.0
         )
 
-    @delayed(2)
     def cancel_order_by_order_id(self, order_id):
         response = httpx.delete(f"{self.BASE_URL}/orders/{order_id}", timeout=10.0)
 
-    @delayed(2)
     def cancel_all_orders(self):
         response = httpx.delete(f"{self.BASE_URL}/orders", timeout=10.0)
 
